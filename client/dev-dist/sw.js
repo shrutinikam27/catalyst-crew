33343e8f5ae0c8ffd289370d9ce8a3457c62db0f
Kaushal <perceptibletons@gmail.com>
2026-05-17 08:16:54 +0530

    feat: initialize service worker with Workbox precaching and navigation routing

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a21gdurtpn"
+    "revision": "0.e1u105g1mms"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
